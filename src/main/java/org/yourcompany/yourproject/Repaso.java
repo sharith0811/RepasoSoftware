7d49fb4720f166832ad427dc1041fbe17cdb95b5
sharith0811 <sharith.santos@estudiantesunibague.edu.co>
2026-02-05 17:53:10 -0500

    Crenado proyecto

---
+++ src/main/java/org/yourcompany/yourproject/Repaso.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author shari
+ */
+public class Repaso {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
