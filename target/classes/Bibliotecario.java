c0046501a9d1743f3d587da797ee135471909c6c
sharith0811 <sharith.santos@estudiantesunibague.edu.co>
2026-02-10 17:21:22 -0500

    Superclase Bibliotecario corregida

--- target/classes/Bibliotecario.java
+++ target/classes/Bibliotecario.java
@@ -1,6 +1,9 @@
 package org.yourcompany.yourproject;
 
 public class Bibliotecario extends Usuario{
+
+    private String turno;
+
     public Bibliotecario(String id, String nombre, String email, String turno){
         super(id, nombre, email);
         this.turno = turno;
