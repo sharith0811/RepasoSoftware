97b9b22ccf11cb672af20c846697404c2126e818
sharith0811 <sharith.santos@estudiantesunibague.edu.co>
2026-02-10 17:19:08 -0500

    Superclase Bibliotecario creada

--- target/classes/Bibliotecario.java
+++ target/classes/Bibliotecario.java
@@ -3,5 +3,10 @@ package org.yourcompany.yourproject;
 public class Bibliotecario extends Usuario{
     public Bibliotecario(String id, String nombre, String email, String turno){
         super(id, nombre, email);
+        this.turno = turno;
+
+    }
+    public String darTurno(){
+        return this.turno;
     }
 }
