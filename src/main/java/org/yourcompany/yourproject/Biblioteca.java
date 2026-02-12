e7180da5c312a283dda629c1bdc88de1cfabf06b
sharith0811 <sharith.santos@estudiantesunibague.edu.co>
2026-02-12 17:11:41 -0500

    Agregar

--- src/main/java/org/yourcompany/yourproject/Biblioteca.java
+++ src/main/java/org/yourcompany/yourproject/Biblioteca.java
@@ -23,10 +23,7 @@ public class Biblioteca {
 
     public void agregarBibliotecario(Bibliotecario bibliotecario){
         bibliotecarios.add(bibliotecario);
-    }
-        
-    
-
-    
+    }    
+       
 
 }
--- src/main/java/org/yourcompany/yourproject/Main.java
+++ src/main/java/org/yourcompany/yourproject/Main.java
@@ -11,10 +11,18 @@ package org.yourcompany.yourproject;
 public class Main {
 
     public static void main(String[] args) {
+
+        Biblioteca biblioteca = new Biblioteca();
+
         Bibliotecario bibliotecario = new Bibliotecario("1", "Pepe", "pepe@gmail.com", "Mañana");
-        System.out.println(bibliotecario.darEmail());
+        
+        biblioteca.agregarBibliotecario(bibliotecario);
 
         Socio socio = new Socio("1", " Juan", "juan@gmail.com", 5);
-        socio.registrarPrestamo(2);
+        
+        biblioteca.agregarSocio(socio);
+
+        System.err.println(biblioteca.darSocios().get(0).darNombre());
+
     }
 }
