4f9c92dd920f7f7ee4e273250ece5d57d3743f10
sharith0811 <sharith.santos@estudiantesunibague.edu.co>
2026-02-12 16:57:56 -0500

    Creación Biblioteca

---
+++ src/main/java/org/yourcompany/yourproject/Biblioteca.java
@@ -0,0 +1,32 @@
+package org.yourcompany.yourproject;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+
+    private ArrayList<Socio> socios = new ArrayList<>();
+    private ArrayList<Bibliotecario> bibliotecarios = new ArrayList<>();
+
+    public Biblioteca(){}
+
+    public ArrayList<Socio> darSocios() {
+        return socios;
+    }
+        
+    public ArrayList<Bibliotecario> darBibliotecarios() {
+        return bibliotecarios;
+    }
+
+    public void agregarSocio(Socio socio){
+        socios.add(socio);
+    }
+
+    public void agregarBibliotecario(Bibliotecario bibliotecario){
+        bibliotecarios.add(bibliotecario);
+    }
+        
+    
+
+    
+
+}
